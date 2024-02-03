4e5b326e225e5af980573c1774c5c3d137b5c30b
Sheddy00 <hei.sheddy.2@gmail.com>
2024-02-03 22:54:04 +0100

    Controller created for User, Document and Document version

--- src/main/java/com/mgt2/backendproject/controller/DocumentVersionController.java
+++ src/main/java/com/mgt2/backendproject/controller/DocumentVersionController.java
@@ -1,7 +1,42 @@
 package com.mgt2.backendproject.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.mgt2.backendproject.model.entity.DocumentVersion;
+import com.mgt2.backendproject.model.entity.User;
+import com.mgt2.backendproject.service.DocumentVersionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class DocumentVersionController {
+
+    @Autowired
+    private DocumentVersionService documentVersionService;
+
+    @GetMapping("/DocumentVersions")
+    public List<DocumentVersion> getAllDocumentVersion() {
+        return documentVersionService.getAllDocumentVersion();
+    }
+
+    @GetMapping("/DocumentVersion/{id}")
+    public Optional<DocumentVersion> getDocumentVersionById(@PathVariable Integer id){
+        return documentVersionService.getDocumentVersionById(id);
+    }
+
+    @PostMapping("/Historique")
+    public DocumentVersion createDocumentVersion(@RequestBody DocumentVersion documentVersion) {
+        return documentVersionService.createDocumentVersion(documentVersion);
+    }
+
+    @PutMapping("/DocumentVersion/update/{id}")
+    public DocumentVersion updateDocumentVersion(@PathVariable Integer id, @RequestBody DocumentVersion updateDocumentVersion) {
+        return documentVersionService.updateDocumentVersion(id, updateDocumentVersion);
+    }
+
+    @DeleteMapping("/DocumentVersion/Delete/{id}")
+    public void deleteDocumentVersionById(@PathVariable Integer id) {
+        documentVersionService.deleteDocumentVersionById(id);
+    }
 }
