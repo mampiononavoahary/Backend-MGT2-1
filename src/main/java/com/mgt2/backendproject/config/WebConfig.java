27daf4693fa16360438d2a41ec49406f71f7490b
Sheddy00 <hei.sheddy.2@gmail.com>
2024-02-05 09:31:17 +0100

    Fix : update configuration

--- src/main/java/com/mgt2/backendproject/config/WebConfig.java
+++ src/main/java/com/mgt2/backendproject/config/WebConfig.java
@@ -7,10 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // A ajouter ici l'url de front
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Origin", "Content-Type", "Accept");
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
     }
 }
